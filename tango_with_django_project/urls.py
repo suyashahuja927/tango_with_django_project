920fff60051f86a80e2f061c40e256655f7a15bd
Suyash Ahuja <2791732a@student.gla.ac.uk>
2023-01-26 14:34:28 +0000

    chapter 3

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,5 +21,6 @@ from rango import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('rango/', include('rango.urls')),
+    path('rango/about/', views.about, name = 'about'),
     path('admin/', admin.site.urls),
 ]
