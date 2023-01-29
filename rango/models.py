be33cb5f3df4318dbdf885aa30def79a3aa2a5eb
Suyash Ahuja <2791732a@student.gla.ac.uk>
2023-01-29 13:39:33 +0000

    5.8 populate_db

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 class Page(models.Model):
